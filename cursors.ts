33845b68d3458620ac888ae27d54597d41dd02dd
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-05 17:28:02 +0100

    Removed unused cursors.ts.

--- cursors.ts
+++
@@ -1,8 +0,0 @@
-
-export let stringValueCursor: bf.ICursor<130> = {
-    key: 'stringValue'
-}                    
-,
-export let numberValueCursor: bf.ICursor<128> = {
-    key: 'numberValue'
-}                    
