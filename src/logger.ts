ef19642e8c434a5fa79fbeca3c29a8d0f1c7339b
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-05 17:28:46 +0100

    Removed not existing params in console printing.

--- src/logger.ts
+++ src/logger.ts
@@ -8,9 +8,9 @@ export interface ILogger {
 
 export let create = (enableDebug: boolean = false, enableInfo: boolean = true, enableWarning: boolean = true, enableError: boolean = true): ILogger => {
     return {
-        info: (message: string, params?: any) => { enableInfo && console.log(`Info: ${message}`, params); },
-        warning: (message: string, params?: any) => { enableWarning && console.log(`Warning: ${message}`, params); },
-        error: (message: string, params?: any) => { enableError && console.log(`Error: ${message}`, params); },
-        debug: (message: string, params?: any) => { enableDebug && console.log(`Debug: ${message}`, params); }
+        info: (message: string, params?: any) => { enableInfo && (params ? console.log(`Info: ${message}`, params) : console.log(`Info: ${message}`)); },
+        warning: (message: string, params?: any) => { enableWarning && (params ? console.log(`Warning: ${message}`, params) : console.log(`Warning: ${message}`)); },
+        error: (message: string, params?: any) => { enableError && (params ? console.log(`Error: ${message}`, params) : console.log(`Error: ${message}`)) },
+        debug: (message: string, params?: any) => { enableDebug && (params ? console.log(`Debug: ${message}`, params) : console.log(`Debug: ${message}`)); }
     }
 } 
